9ec34d6e817f622d32d43ff56f293ab729250742
XXGGG <534464179@qq.com>
2023-12-31 00:50:41 +0800

    🚀 起飞喽

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-   
+    <div class="Home">
+        <div p-20 i-line-md-moon-filled-alt-loop dark:i-line-md-moon-filled-alt-to-sunny-filled-loop-transition
+            @click="toggleDark" class="toggleBtn" />
+    </div>
 </template>
 
 <script setup lang='ts'>
+import { toggleDark } from '~/logics'
 import { useDark, useToggle } from '@vueuse/core'
 
 // 使用 Vueuse的两个函数来实现切换【暗黑模式】
@@ -10,3 +14,16 @@ const isDark = useDark()
 // const toggleDark = useToggle(isDark)
 </script>
 
+<style scoped>
+.Home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+}
+
+.toggleBtn {
+    cursor: pointer;
+}
+</style>
