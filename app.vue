d19fe4e9db89e8260841eb15eb7a16254a2f8f8d
XXGGG <534464179@qq.com>
2024-01-03 03:08:47 +0800

    😠 这会估计可以了

--- app.vue
+++ app.vue
@@ -1,26 +1,18 @@
 <template>
+  <div>
     <div class="top">
-              <RouterLink to="/" class="logo" p-4 i-tabler-tools></RouterLink>
-              <div>XGTools</div>
-              <div p-4 i-line-md-moon-filled-alt-loop dark:i-line-md-moon-filled-alt-to-sunny-filled-loop-transition
-                  @click="toggleDark" class="toggleBtn" />
-          </div>
+        <RouterLink to="/" class="logo" p-4 i-tabler-tools></RouterLink>
+        <div>XGTools</div>
+        <div p-4 i-line-md-moon-filled-alt-loop dark:i-line-md-moon-filled-alt-to-sunny-filled-loop-transition
+            @click="toggleDark" class="toggleBtn" />
+    </div>
     <NuxtPage />
+  </div>
 </template>
 <script setup lang='ts'>
 import { toggleDark } from '~/logics'
-import { useDark, useToggle } from '@vueuse/core'
-
-
-const go_router = (url: string) => {
-  navigateTo(url)
-}
-// 使用 Vueuse的两个函数来实现切换【暗黑模式】
-const isDark = useDark()
-  // const toggleDark = useToggle(isDark)
 </script>
 <style>
-
 body {  
   background-color: #fff;
   color: rgba(0, 0, 0, 0.8);
@@ -32,7 +24,6 @@ body {
   color: rgba(255, 255, 255, 0.8);
   background-image: url('/bg-dark.svg');
 }
-
 .top {
     width: calc(100vw - 20px);
     margin: 10px;
@@ -48,7 +39,6 @@ body {
 .toggleBtn {
     cursor: pointer;
 }
-
 .page-enter-active,
 .page-leave-active {
   transition: all 0.4s;
